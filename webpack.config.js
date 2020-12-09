7a8024307f30774b2d5903856f9c8d27a1bb00ae
Flichyn <70525056+Flichyn@users.noreply.github.com>
2020-12-09 11:19:45 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    .addEntry('about-us', './assets/styles/about-us.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
