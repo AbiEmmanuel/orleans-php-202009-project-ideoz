93186d6b6d27cf8c9b1279d8a04ff89a8b3f0d05
Damien Changenot <damien.changenot@gmail.com>
2020-12-10 16:57:54 +0100

    fix fixture +migrations

--- src/DataFixtures/CompanyFixtures.php
+++ src/DataFixtures/CompanyFixtures.php
@@ -9,12 +9,14 @@ use Doctrine\Persistence\ObjectManager;
 
 class CompanyFixtures extends Fixture
 {
+    private const NAME = 'Simon Abraham';
+    private const PHONE_NUMBER = '0661992159'
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
         $company = new Company();
-        $company->setName($faker->word);
-        $company->setPhoneNumber($faker->phoneNumber);
+        $company->setName(self::NAME);
+        $company->setPhoneNumber(self::PHONE_NUMBER);
         $company->setEmail($faker->email);
         $manager->persist($company);
         $manager->flush();
