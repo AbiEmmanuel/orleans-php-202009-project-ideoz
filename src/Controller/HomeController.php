adaa7125921eae238a5c28a2633de5b94a4f2af4
Gambino-stack <69907371+Gambino-stack@users.noreply.github.com>
2020-12-16 14:15:49 +0100

    add testimonies page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Testimony;
+use App\Repository\TestimonyRepository;
 use App\Repository\OfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,13 +13,15 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param TestimonyRepository $testimonyRepository
      * @param OfferRepository $offerRepository
      * @return Response
      */
-    public function index(OfferRepository $offerRepository): Response
+    public function index(TestimonyRepository $testimonyRepository, OfferRepository $offerRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'services' => $offerRepository->findAll(),
+          'testimonies' => $testimonyRepository->findBy([], ['id' => 'DESC'], 4),
         ]);
     }
 }
