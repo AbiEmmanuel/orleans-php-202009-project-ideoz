d4f9b2537384eb2026b4d98dea4d9b41b180d72b
Damien Changenot <damien.changenot@gmail.com>
2020-12-16 21:29:18 +0100

    index admintestimony

---
+++ src/Controller/TestimonyController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Testimony;
+use App\Form\TestimonyType;
+use App\Repository\TestimonyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("/testimony")
+*/
+class TestimonyController extends AbstractController
+{
+    /**
+    * @Route("/admin", name="admin_testimony_index", methods={"GET"})
+    * @param TestimonyRepository $testimonyRepository
+    * @return Response
+    */
+    public function index(TestimonyRepository $testimonyRepository): Response
+    {
+        return $this->render('adminTestimony/index.html.twig', [
+        'testimonies' => $testimonyRepository->findAll(),
+        ]);
+    }
+}
