3ec9b2dda22e2404541fcdea42fd334aa379d979
Gambino-stack <69907371+Gambino-stack@users.noreply.github.com>
2021-01-06 11:02:55 +0100

    change an Assert

--- migrations/Version20201215092848.php
+++ migrations/Version20201215092848.php
@@ -22,7 +22,7 @@ final class Version20201215092848 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, phone_number VARCHAR(45) DEFAULT NULL, email VARCHAR(255) DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE ecosystem (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, logo VARCHAR(255) DEFAULT NULL, status TINYINT(1) NOT NULL, activity VARCHAR(255) DEFAULT NULL, particularity VARCHAR(255) DEFAULT NULL, url VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE adminOffer (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, description LONGTEXT NOT NULL, abstract LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE Offer (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, description LONGTEXT NOT NULL, abstract LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE testimony (id INT AUTO_INCREMENT NOT NULL, ecosystem_id INT NOT NULL, content LONGTEXT NOT NULL, UNIQUE INDEX UNIQ_523C9487146249B8 (ecosystem_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE testimony ADD CONSTRAINT FK_523C9487146249B8 FOREIGN KEY (ecosystem_id) REFERENCES ecosystem (id)');
     }
@@ -33,7 +33,7 @@ final class Version20201215092848 extends AbstractMigration
         $this->addSql('ALTER TABLE testimony DROP FOREIGN KEY FK_523C9487146249B8');
         $this->addSql('DROP TABLE company');
         $this->addSql('DROP TABLE ecosystem');
-        $this->addSql('DROP TABLE adminOffer');
+        $this->addSql('DROP TABLE Offer');
         $this->addSql('DROP TABLE testimony');
     }
 }
