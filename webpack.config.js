a97f80ec08f5beafb3beb26db5bb536bbbde59a4
AmelieRoussel <amelie.roussel41@gmail.com>
2021-01-18 11:16:28 +0100

    create admin homepage + add member section on it

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('home', './assets/js/home.js')
+    .addEntry('admin', './assets/styles/admin.scss')
     .addEntry('adminOffer', './assets/styles/adminOffer.scss')
     .addEntry('testimony', './assets/styles/testimony.scss')
     .addEntry('adminEcosystem', './assets/styles/adminEcosystem.scss')
