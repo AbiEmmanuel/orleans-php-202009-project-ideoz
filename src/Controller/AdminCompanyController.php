65a2c8cefb08df164584bf339a71b5cca964a43e
Damien Changenot <damien.changenot@gmail.com>
2021-01-18 11:21:37 +0100

    edit

--- src/Controller/AdminCompanyController.php
+++ src/Controller/AdminCompanyController.php
@@ -26,4 +26,27 @@ class AdminCompanyController extends AbstractController
             'companies' => $companyRepository->findAll(),
         ]);
     }
+
+    /**
+     * @Route("/{id}/edit", name="company_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Company $company
+     * @return Response
+     */
+    public function edit(Request $request, Company $company): Response
+    {
+        $form = $this->createForm(CompanyType::class, $company);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('adminCompany_index');
+        }
+
+        return $this->render('company/edit.html.twig', [
+            'company' => $company,
+            'form' => $form->createView(),
+        ]);
+    }
 }
