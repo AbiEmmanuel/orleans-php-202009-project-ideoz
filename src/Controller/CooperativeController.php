cf69f5caee515a93d664cfd88f7791acbda5d3d9
Flichyn <nicolasflichy@gmail.com>
2021-01-20 14:43:35 +0100

    Refixed the route

--- src/Controller/CooperativeController.php
+++ src/Controller/CooperativeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/espace_co-oz", name="cooperative_")
+ * @Route("/espace-cooz", name="cooperative_")
  */
 class CooperativeController extends AbstractController
 {
