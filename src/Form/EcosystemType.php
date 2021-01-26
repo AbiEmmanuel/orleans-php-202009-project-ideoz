da092ed2f823fe41e4168014d7e1bbaaee44676d
Amelie <70197706+AmelieRoussel@users.noreply.github.com>
2021-01-26 10:57:14 +0100

    Update EcosystemType.php

--- src/Form/EcosystemType.php
+++ src/Form/EcosystemType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Ecosystem;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
